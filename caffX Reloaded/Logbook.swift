1072b5418dea10582bf1671c04edb85a9f320ec2
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2024-09-18 08:22:44 +0800

    Overhaul

---
+++ caffX Reloaded/Logbook.swift
@@ -0,0 +1,18 @@
+//
+//  Logbook.swift
+//  caffX Reloaded
+//
+//  Created by Avyan Mehra on 16/9/24.
+//
+
+import SwiftUI
+
+struct Logbook: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logbook()
+}
---
+++ caffX Reloaded/dailyGraph.swift
@@ -0,0 +1,18 @@
+//
+//  dailyGraph.swift
+//  caffX Reloaded
+//
+//  Created by Avyan Mehra on 18/9/24.
+//
+
+import SwiftUI
+
+struct dailyGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dailyGraph()
+}
